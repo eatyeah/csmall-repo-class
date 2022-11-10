bf6ebf946d3821e1475487c6b1d29c5f7e0b1991
jtzhanghl <jtzhanghl@tedu.cn>
2022-11-10 14:56:35 +0800

    秒杀业务

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillServiceImpl.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillServiceImpl.java
@@ -1,14 +1,20 @@
 package cn.tedu.mall.seckill.service.impl;
 
+import cn.tedu.mall.common.exception.CoolSharkServiceException;
+import cn.tedu.mall.common.pojo.domain.CsmallAuthenticationInfo;
+import cn.tedu.mall.common.restful.ResponseCode;
 import cn.tedu.mall.order.service.IOmsOrderService;
 import cn.tedu.mall.pojo.seckill.dto.SeckillOrderAddDTO;
 import cn.tedu.mall.pojo.seckill.vo.SeckillCommitVO;
 import cn.tedu.mall.seckill.service.ISeckillService;
+import cn.tedu.mall.seckill.utils.SeckillCacheUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,8 +31,48 @@ public class SeckillServiceImpl implements ISeckillService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    /*
+    1.判断用户是否为重复购买和Redis中该Sku是否有库存
+    2.秒杀订单转换成普通订单,需要使用dubbo调用order模块的生成订单方法
+    3.使用消息队列(RabbitMQ)将秒杀成功记录信息保存到success表中
+    4.秒杀订单信息返回
+     */
     @Override
     public SeckillCommitVO commitSeckill(SeckillOrderAddDTO seckillOrderAddDTO) {
+        // 第一阶段:判断用户是否为重复购买和Redis中该Sku是否有库存
+        // 从方法的参数中,获得用户想要购买的skuId
+        Long skuId=seckillOrderAddDTO.getSeckillOrderItemAddDTO().getSkuId();
+        // 从SpringSecurity上下文中获取登录用户的Id
+        Long userId=getUserId();
+        // 我们明确了本次请求时哪个用户要购买哪个商品(userId以及skuId的具体值)
+        // 根据我们的秒杀业务规定,每个用户只能购买skuId一次
+        // 所以可以依据userId和skuId生成检查重复购买的Key
+        // mall:seckill:reseckill:2:1
+        String reSeckillCheckKey= SeckillCacheUtils.getReseckillCheckKey(skuId,userId);
         return null;
+    }
+
+
+    public CsmallAuthenticationInfo getUserInfo(){
+        // 编写SpringSecurity上下文中获得用户信息的代码
+        UsernamePasswordAuthenticationToken authenticationToken=
+                (UsernamePasswordAuthenticationToken)
+                        SecurityContextHolder.getContext().getAuthentication();
+        // 为了逻辑严谨性,判断一下SpringSecurity上下文中的信息是不是null
+        if(authenticationToken == null){
+            throw new CoolSharkServiceException(
+                    ResponseCode.UNAUTHORIZED,"您没有登录!");
+        }
+        // 确定authenticationToken不为null
+        // 就可以从中获得用户信息了
+        CsmallAuthenticationInfo csmallAuthenticationInfo=
+                (CsmallAuthenticationInfo) authenticationToken.getCredentials();
+        // 别忘了返回
+        return csmallAuthenticationInfo;
+    }
+    // 业务逻辑层中的方法实际上都只需要用户的id即可
+    // 我们可以再编写一个方法,从用户对象中获得id
+    public Long getUserId(){
+        return getUserInfo().getId();
     }
 }

fc653531e359bbb5cc62999a938a11464adb8020
jtzhanghl <jtzhanghl@tedu.cn>
2022-11-10 15:56:02 +0800

    秒杀业务

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillServiceImpl.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillServiceImpl.java
@@ -4,6 +4,7 @@ import cn.tedu.mall.common.exception.CoolSharkServiceException;
 import cn.tedu.mall.common.pojo.domain.CsmallAuthenticationInfo;
 import cn.tedu.mall.common.restful.ResponseCode;
 import cn.tedu.mall.order.service.IOmsOrderService;
+import cn.tedu.mall.pojo.order.dto.OrderAddDTO;
 import cn.tedu.mall.pojo.seckill.dto.SeckillOrderAddDTO;
 import cn.tedu.mall.pojo.seckill.vo.SeckillCommitVO;
 import cn.tedu.mall.seckill.service.ISeckillService;
@@ -11,6 +12,7 @@ import cn.tedu.mall.seckill.utils.SeckillCacheUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -49,6 +51,57 @@ public class SeckillServiceImpl implements ISeckillService {
         // 所以可以依据userId和skuId生成检查重复购买的Key
         // mall:seckill:reseckill:2:1
         String reSeckillCheckKey= SeckillCacheUtils.getReseckillCheckKey(skuId,userId);
+        // 以上面字符串为key,调用stringRedisTemplate中的increment方法
+        // increment是增长的意思
+        // 1.如果上面的key从没在Redis中保存过,redis中会创建这个key,并保存它的值为1
+        // 2.如果上面的key已经在redis中,那么就会在当前的值基础上加1,再保存,如果当前是1,运行后变为2
+        // 3.无论存不存在这个key,运行后都会返回值
+        // 所以如果返回的值为1,就证明这个用户之前从未购买过这个商品
+        Long seckillTimes=stringRedisTemplate
+                .boundValueOps(reSeckillCheckKey).increment();
+        // 如果seckillTimes值为1就可以购买,大于1就不能购买
+        if(seckillTimes>1){
+            // 抛出异常,提示不能重复购买,终止程序
+            throw new CoolSharkServiceException(ResponseCode.FORBIDDEN,
+                    "您已经购买过这个商品了,谢谢您的支持!");
+        }
+        // 程序运行到此处,表示用户第一次购买这个商品
+        // 然后去检查是否有库存
+        // 确定要购买的skuId,生成获得这个skuId已经预热的库存数的key
+        // mall:seckill:sku:stock:1
+        String skuStockKey=SeckillCacheUtils.getStockKey(skuId);
+        // 根据上面的key在redis中调用decrement(减少)的方法,将库存数-1后返回
+        Long leftStock=stringRedisTemplate
+                .boundValueOps(skuStockKey).decrement();
+        // leftStock是decrement方法减1之后返回的
+        // 所以我们获得的leftStock是如果当前用户购买成功剩余的库存数
+        // 如果leftStock是0,表示当前用户买到最后一个,如果小于0,才是没有库存,表示售罄
+        if(leftStock<0){
+            // 如果已经没有库存,就要终止本次用户购买
+            // 要将当前用户购买此商品的次数恢复为0,下次该用户还能购买
+            stringRedisTemplate.boundValueOps(reSeckillCheckKey).decrement();
+            // 抛出异常
+            throw new CoolSharkServiceException(ResponseCode.BAD_REQUEST,
+                    "对不起,您要购买的商品暂时售罄");
+        }
+        // 到此为止,当前用户经过了重复购买的检查和库存的判断,可以开始生成订单了
+        // 第二阶段:秒杀订单转换成普通订单,需要使用dubbo调用order模块的生成订单方法
+        // 当前方法参数:SeckillOrderAddDTO,dubbo调用需要的参数:OrderAddDTO
+        // 下面开始转换,转换代码较多,单独编写一个方法
+        OrderAddDTO orderAddDTO=convertSeckillOrderToOrder(seckillOrderAddDTO);
+
+        return null;
+    }
+    // 秒杀订单,转换为普通定的方法
+    private OrderAddDTO convertSeckillOrderToOrder(SeckillOrderAddDTO seckillOrderAddDTO) {
+        // 先实例化返回值类型对象
+        OrderAddDTO orderAddDTO=new OrderAddDTO();
+        // 将同名属性赋值到orderAddDTO对象
+        BeanUtils.copyProperties(seckillOrderAddDTO,orderAddDTO);
+        // 这两个订单对象基本属性都相同,区别在于
+        // orderAddDTO普通订单对象,其中的订单项是一个集合:List<OrderItemAddDTO>
+        // seckillOrderAddDTO秒杀订单对象,其中的秒杀订单项是一个对象:SeckillOrderItemAddDTO
+
         return null;
     }
 

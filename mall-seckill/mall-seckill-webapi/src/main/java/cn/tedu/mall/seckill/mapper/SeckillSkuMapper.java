536391d973b2b69fd6b2133f6f99e844d96c114a
h-qian1 <2533897869@qq.com>
2022-11-11 09:49:34 +0800

    搜索功能

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
@@ -1,6 +1,7 @@
 package cn.tedu.mall.seckill.mapper;
 
 import cn.tedu.mall.pojo.seckill.model.SeckillSku;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,10 @@ public interface SeckillSkuMapper {
 
     // 根据spuId查询sku列表
     List<SeckillSku> findSeckillSkusBySpuId(Long spuId);
+
+    // 根据skuId减少库存的方法
+    int updateReduceStockBySkuId(@Param("skuId") Long  skuId,
+                                 @Param("quantity") Integer quantity);
 
 
 }

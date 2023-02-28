50bb9d7440597825448b6abfe5cc87da352859ed
sweeterjava <sweeterjava@163.com>
2023-02-28 14:44:09 +0800

    提交代码

---
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontCategoryServiceImpl.java
@@ -0,0 +1,22 @@
+package cn.tedu.mall.front.service.impl;
+
+import cn.tedu.mall.front.service.IFrontCategoryService;
+import cn.tedu.mall.pojo.front.vo.FrontCategoryTreeVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:
+ * @Author: sweeterjava@163.com
+ * @Date: 2023/2/28
+ * @Time: 14:42
+ */
+
+@Service
+@Slf4j
+public class FrontCategoryServiceImpl implements IFrontCategoryService {
+    @Override
+    public FrontCategoryTreeVO categoryTree() {
+        return null;
+    }
+}

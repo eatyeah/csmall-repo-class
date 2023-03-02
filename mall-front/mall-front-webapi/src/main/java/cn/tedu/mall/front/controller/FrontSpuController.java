8ea6304299c02d271682c5a2d3c2e0442731c276
sweeterjava <sweeterjava@163.com>
2023-03-02 19:34:09 +0800

    提交代码

--- mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/controller/FrontSpuController.java
@@ -3,7 +3,10 @@ package cn.tedu.mall.front.controller;
 import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.common.restful.JsonResult;
 import cn.tedu.mall.front.service.IFrontProductService;
+import cn.tedu.mall.pojo.product.vo.AttributeStandardVO;
+import cn.tedu.mall.pojo.product.vo.SkuStandardVO;
 import cn.tedu.mall.pojo.product.vo.SpuListItemVO;
+import cn.tedu.mall.pojo.product.vo.SpuStandardVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Description:
@@ -42,4 +47,33 @@ public class FrontSpuController {
                 .listSpuByCategoryId(categoryId, page, pageSize);
         return JsonResult.ok(jsonPage);
     }
+
+    // localhost:10004/front/spu/1
+    @GetMapping("/{spuId}")
+    @ApiOperation("根据spuId查询spu信息")
+    @ApiImplicitParam(value = "spuId", name="spuId",example = "1")
+    public JsonResult<SpuStandardVO> getFrontSkuById(@PathVariable Long spuId){
+        SpuStandardVO spuStandardVO = frontProductService.getFrontSpuById(spuId);
+        return JsonResult.ok(spuStandardVO);
+    }
+
+    // localhost:10004/front/spu/template/1
+    @GetMapping("/template/{id}")
+    @ApiOperation("根据spuId查询所有的属性和规格")
+    @ApiImplicitParam(value = "spuId",name = "id",example = "1")
+    public JsonResult<List<AttributeStandardVO>> getAttributesById(@PathVariable Long id){
+        List<AttributeStandardVO> list = frontProductService.getSpuAttributesBySpuId(id);
+        return JsonResult.ok(list);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+

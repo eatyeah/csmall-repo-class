5c880e0d8650b9b89179cc31eb1474e52b164185
sweeterjava <sweeterjava@163.com>
2023-03-03 15:02:20 +0800

    提交代码

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
@@ -1,10 +1,25 @@
 package cn.tedu.mall.order.mapper;
 
+import cn.tedu.mall.pojo.order.model.OmsCart;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
 /**
  * @Description:
  * @Author: sweeterjava@163.com
  * @Date: 2023/3/3
  * @Time: 14:25
  */
+
+@Repository
 public interface OmsCartMapper {
+    // 判断当前用户的购物车中是否存在指定商品
+    OmsCart selectExistsCart(@Param("userId") Long userId,
+                             @Param("skuId") Long skuId);
+
+    // 新增sku信息到购物车
+    int saveCart(OmsCart omsCart);
+
+    // 修改购物车中的sku商品数量
+    int updateQuantityById(OmsCart omsCart);
 }

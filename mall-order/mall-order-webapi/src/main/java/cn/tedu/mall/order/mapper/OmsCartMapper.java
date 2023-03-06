bb3d215eeff970013b6520e746569de4113cf814
sweeterjava <sweeterjava@163.com>
2023-03-06 10:16:33 +0800

    提交代码

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/mapper/OmsCartMapper.java
@@ -1,8 +1,11 @@
 package cn.tedu.mall.order.mapper;
 
 import cn.tedu.mall.pojo.order.model.OmsCart;
+import cn.tedu.mall.pojo.order.vo.CartStandardVO;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @Description:
@@ -22,4 +25,11 @@ public interface OmsCartMapper {
 
     // 修改购物车中的sku商品数量
     int updateQuantityById(OmsCart omsCart);
+
+    // 根据用户id查询该用户购物车中的sku信息
+    List<CartStandardVO> selectCartByUserId(Long userId);
+
+    // 根据用户选中的一个或多个id,删除购物车中的商品(支持批量删除)
+    int deleteCartsByIds(Long[] ids);
+
 }

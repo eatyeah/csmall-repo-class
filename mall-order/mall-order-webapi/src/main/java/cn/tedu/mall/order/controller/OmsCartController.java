8b4fee46ae1c2cffedfaf154b386c29d10a24ad9
sweeterjava <sweeterjava@163.com>
2023-03-06 10:16:30 +0800

    提交代码

--- mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/controller/OmsCartController.java
+++ mall-order/mall-order-webapi/src/main/java/cn/tedu/mall/order/controller/OmsCartController.java
@@ -1,13 +1,18 @@
 package cn.tedu.mall.order.controller;
 
+import cn.tedu.mall.common.restful.JsonPage;
 import cn.tedu.mall.common.restful.JsonResult;
 import cn.tedu.mall.order.service.IOmsCartService;
 import cn.tedu.mall.pojo.order.dto.CartAddDTO;
+import cn.tedu.mall.pojo.order.vo.CartStandardVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,10 +45,31 @@ public class OmsCartController {
     // @Validated注解是激活SpringValidation框架用的
     // 参数CartAddDTO中,各个属性设置了验证规则,如果有参数值不符合规则
     // 会抛出BindException异常,由统一异常处理类处理,控制方法就不会运行了
-    public JsonResult addCart(@Validated CartAddDTO cartAddDTO){
+    public JsonResult addCart(@Validated CartAddDTO cartAddDTO) {
         omsCartService.addCart(cartAddDTO);
         return JsonResult.ok("新增sku到购物车完成!");
 
     }
 
+
+    @GetMapping("/list")
+    @ApiOperation("根据用户Id分页查询购物车sku列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(value = "页码", name = "page", example = "1"),
+            @ApiImplicitParam(value = "每页条数", name = "pageSize", example = "10")
+    })
+    @PreAuthorize("hasAnyAuthority('ROLE_user')")
+    public JsonResult<JsonPage<CartStandardVO>> listCartByPage(Integer page, Integer pageSize) {
+        JsonPage<CartStandardVO> jsonPage = omsCartService.listCarts(page, pageSize);
+        return JsonResult.ok(jsonPage);
+    }
+
+    @PostMapping("/delete")
+    @ApiOperation("根据id数组删除购物车sku信息")
+    @ApiImplicitParam(value = "要删除的id数组", name = "ids", required = true, dataType = "arrray")
+    @PreAuthorize("hasAnyAuthority('ROLE_user')")
+    public JsonResult removeCartByIds(Long[] ids){
+        omsCartService.removeCart(ids);
+        return JsonResult.ok("删除完成！");
+    }
 }
